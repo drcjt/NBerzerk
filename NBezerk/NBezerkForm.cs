884bccc35d312554a263a58eec6a29c9bbb86bd4
drcjt <drcjt@hotmail.com>
2013-07-24 21:51:33 +0100

    Add player and simple controls

--- NBezerk/NBezerkForm.cs
+++ NBezerk/NBezerkForm.cs
@@ -15,6 +15,8 @@ namespace NBezerk
         SolidBrush wallBrush = new SolidBrush(Color.FromArgb(0, 0, 108));
 
         Bitmap backBuffer;
+        Image player;
+        Point playerPosition = new Point(100, 100);
 
         string maze;
 
@@ -24,6 +26,8 @@ namespace NBezerk
 
             SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.DoubleBuffer, true);
 
+            player = LoadImage("player.png");
+
             UInt16 room = RandomNumberGenerator.GetRandomNumber(0);
             maze = MazeGenerator.GenerateMaze(room);
 
@@ -32,9 +36,63 @@ namespace NBezerk
             GameTimer.Tick += new EventHandler(GameTick);
             GameTimer.Start();
 
+            KeyDown += new KeyEventHandler(KeyDownHandler);
+            KeyUp += new KeyEventHandler(KeyUpHandler);
+
             Load += new EventHandler(CreateBackBuffer);
             Paint += new PaintEventHandler(GamePaint);
         }
+
+        bool isUpPressed = false;
+        bool isDownPressed = false;
+        bool isLeftPressed = false;
+        bool isRightPressed = false;
+
+        void KeyDownHandler(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    isUpPressed = true;
+                    break;
+
+                case Keys.Down:
+                    isDownPressed = true;
+                    break;
+
+                case Keys.Left:
+                    isLeftPressed = true;
+                    break;
+
+                case Keys.Right:
+                    isRightPressed = true;
+                    break;
+            }
+        }
+
+
+        void KeyUpHandler(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    isUpPressed = false;
+                    break;
+
+                case Keys.Down:
+                    isDownPressed = false;
+                    break;
+
+                case Keys.Left:
+                    isLeftPressed = false;
+                    break;
+
+                case Keys.Right:
+                    isRightPressed = false;
+                    break;
+            }
+        }
+
 
         void CreateBackBuffer(object sender, EventArgs e)
         {
@@ -55,42 +113,46 @@ namespace NBezerk
             }
         }
 
-        void DrawRoom()
+        void DrawPlayer(Graphics g)
         {
-            using (var g = Graphics.FromImage(backBuffer))
+            Rectangle destRect = new Rectangle(playerPosition, new Size(8, 16));
+            Rectangle srcRect = new Rectangle(0, 0, 8, 16);
+            g.DrawImage(player, destRect, srcRect, GraphicsUnit.Pixel);
+            //Image i = new 
+            //
+        }
+
+        void DrawRoom(Graphics g)
+        {
+            // Draw top walls
+            g.FillRectangle(wallBrush, 4, 0, 99, 4);
+            g.FillRectangle(wallBrush, 152, 0, 99, 4);
+
+            // Draw bottom walls
+            g.FillRectangle(wallBrush, 4, 204, 99, 4);
+            g.FillRectangle(wallBrush, 152, 204, 99, 4);
+
+            // Draw left walls
+            g.FillRectangle(wallBrush, 4, 0, 4, 71);
+            g.FillRectangle(wallBrush, 4, 136, 4, 71);
+
+            // Draw right walls
+            g.FillRectangle(wallBrush, 248, 0, 4, 71);
+            g.FillRectangle(wallBrush, 248, 136, 4, 71);
+
+            for (int pillarIndex = 0; pillarIndex < 8; pillarIndex++)
             {
-                g.Clear(Color.Black);
+                Point pillarLocation = new Point(56 + (pillarIndex % 4) * 48, pillarIndex < 4 ? 68 : 136);
 
-                // Draw top walls
-                g.FillRectangle(wallBrush, 4, 0, 99, 4);
-                g.FillRectangle(wallBrush, 152, 0, 99, 4);
+                char wallDirection = maze[pillarIndex];
 
-                // Draw bottom walls
-                g.FillRectangle(wallBrush, 4, 204, 99, 4);
-                g.FillRectangle(wallBrush, 152, 204, 99, 4);
+                Rectangle wallRectangle = new Rectangle();
+                wallRectangle.X = (wallDirection == 'W') ? pillarLocation.X - 52 : pillarLocation.X;
+                wallRectangle.Y = (wallDirection == 'N') ? pillarLocation.Y - 67 : pillarLocation.Y;
+                wallRectangle.Width = (wallDirection == 'N' || wallDirection == 'S') ? 4 : 52;
+                wallRectangle.Height = (wallDirection == 'N' || wallDirection == 'S') ? 71 : 4;
 
-                // Draw left walls
-                g.FillRectangle(wallBrush, 4, 0, 4, 71);
-                g.FillRectangle(wallBrush, 4, 136, 4, 71);
-
-                // Draw right walls
-                g.FillRectangle(wallBrush, 248, 0, 4, 71);
-                g.FillRectangle(wallBrush, 248, 136, 4, 71);
-
-                for (int pillarIndex = 0; pillarIndex < 8; pillarIndex++)
-                {
-                    Point pillarLocation = new Point(56 + (pillarIndex % 4) * 48, pillarIndex < 4 ? 68 : 136);
-
-                    char wallDirection = maze[pillarIndex];
-
-                    Rectangle wallRectangle = new Rectangle();
-                    wallRectangle.X = (wallDirection == 'W') ? pillarLocation.X - 52 : pillarLocation.X;
-                    wallRectangle.Y = (wallDirection == 'N') ? pillarLocation.Y - 67 : pillarLocation.Y;
-                    wallRectangle.Width = (wallDirection == 'N' || wallDirection == 'S') ? 4 : 52;
-                    wallRectangle.Height = (wallDirection == 'N' || wallDirection == 'S') ? 71 : 4;
-
-                    g.FillRectangle(wallBrush, wallRectangle);
-                }
+                g.FillRectangle(wallBrush, wallRectangle);
             }
         }
 
@@ -98,9 +160,41 @@ namespace NBezerk
         {
             if (backBuffer != null)
             {
-                DrawRoom();
+                using (var g = Graphics.FromImage(backBuffer))
+                {
+                    g.Clear(Color.Black);
+
+                    DrawRoom(g);
+                    DrawPlayer(g);
+                }
+
                 Invalidate();
             }
+
+            if (isUpPressed)
+            {
+                playerPosition.Y = playerPosition.Y - 3;
+            }
+            if (isDownPressed)
+            {
+                playerPosition.Y = playerPosition.Y + 3;
+            }
+            if (isLeftPressed)
+            {
+                playerPosition.X = playerPosition.X - 3;
+            }
+            if (isRightPressed)
+            {
+                playerPosition.X = playerPosition.X + 3;
+            }
+        }
+
+        Image LoadImage(string imageFile)
+        {
+            var thisExe = System.Reflection.Assembly.GetExecutingAssembly();
+            string[] names = thisExe.GetManifestResourceNames();
+            System.IO.Stream file = thisExe.GetManifestResourceStream("NBezerk.Resources." + imageFile);
+            return Image.FromStream(file);
         }
     }
 }
